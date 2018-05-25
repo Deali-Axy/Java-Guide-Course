31d914d29f90aea2b951175212414668fda45e82
DealiAxy <dealiaxy@gmail.com>
2018-05-25 09:45:04 +0800

    开始玩Swing图形界面

---
+++ src/da/section14/Tutorials/HelloWorld.java
@@ -0,0 +1,17 @@
+package da.section14.Tutorials;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        JFrame frame = new JFrame("HelloWorld");
+        JLabel label = new JLabel("HelloWorld", SwingConstants.CENTER);
+        label.setFont(new Font("Consolas", 1, 20));
+        frame.setSize(300, 100);
+        frame.add(label);
+        frame.setLocationRelativeTo(null);  //put it on the center screen
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}

326205578d2781f9dca2c37cb7c8f197c698f078
DealiAxy <dealiaxy@gmail.com>
2018-11-06 22:22:49 +0800

    实验六和七，还没做，待会回宿舍再做了。

---
+++ src/lin/Experiment/Exp6/T1/Cylinder.java
@@ -0,0 +1,13 @@
+package lin.Experiment.Exp6.T1;
+
+public class Cylinder implements  Shape {
+    @Override
+    public float volume() {
+        return 0;
+    }
+
+    @Override
+    public float area() {
+        return 0;
+    }
+}
---
+++ src/lin/Experiment/Exp6/T1/Shape.java
@@ -0,0 +1,17 @@
+package lin.Experiment.Exp6.T1;
+
+public interface Shape {
+    /**
+     * 计算体积
+     *
+     * @return 体积
+     */
+    float volume();
+
+    /**
+     * 计算表面积
+     *
+     * @return 表面积
+     */
+    float area();
+}
